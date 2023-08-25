c75d14d248e7d5c032a74cb6a274375e712fb954
Elaidzha1940 <eli080293@gmail.com>
2023-08-25 17:14:48 +0300

    BlurredText

---
+++ BlurredText/BlurredTextApp.swift
@@ -0,0 +1,21 @@
+//  /*
+//
+//  Project: BlurredText
+//  File: BlurredTextApp.swift
+//  Created by: Elaidzha Shchukin
+//  Date: 25.08.2023
+//
+//  Status: in progress | Decorated
+//
+//  */
+
+import SwiftUI
+
+@main
+struct BlurredTextApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BlurredText/ContentView.swift
@@ -0,0 +1,28 @@
+//  /*
+//
+//  Project: BlurredText
+//  File: ContentView.swift
+//  Created by: Elaidzha Shchukin
+//  Date: 25.08.2023.
+//
+//  Status: in progress | Decorated
+//
+//  */
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        
+        VStack {
+            
+            
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
